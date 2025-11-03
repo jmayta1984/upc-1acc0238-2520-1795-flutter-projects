416df69c4b399781172dc771fe5446fae4e32063
jmayta1984 <jmayta@pucp.edu.pe>
2025-11-02 19:23:13 -0500

    refactor(main): clean theme initialization and bloc setup

--- easy_travel/lib/main.dart
+++ easy_travel/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:easy_travel/features/auth/register_page.dart';
 import 'package:easy_travel/core/ui/theme.dart';
-import 'package:easy_travel/features/home/data/destination_service.dart';
+import 'package:easy_travel/features/home/data/repositories/destination_repository_impl.dart';
+import 'package:easy_travel/features/home/data/services/destination_service.dart';
 import 'package:easy_travel/features/home/domain/models/category.dart';
 import 'package:easy_travel/features/home/presentation/blocs/home_bloc.dart';
 import 'package:easy_travel/features/home/presentation/blocs/home_event.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+final MaterialTheme appMaterialTheme = MaterialTheme(
+  ThemeData(useMaterial3: true).textTheme,
+);
 
 void main() {
   runApp(const MainApp());
@@ -16,18 +21,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final MaterialTheme theme = MaterialTheme(TextTheme());
+    final theme = appMaterialTheme;
 
     return BlocProvider(
-      create: (context) =>
-          HomeBloc(service: DestinationService())
-            ..add(GetDestinationsByCategory(category: CategoryType.all)),
+      create: (context) => HomeBloc(
+        repository: DestinationRepositoryImpl(service: DestinationService()),
+      )..add(GetDestinationsByCategory(category: CategoryType.all)),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        highContrastTheme: theme.lightHighContrast(),
         theme: theme.light(),
         darkTheme: theme.dark(),
-        home: Scaffold(body: SafeArea(child: RegisterPage())),
+        themeMode: ThemeMode.system,
+        highContrastTheme: theme.lightHighContrast(),
+        home: const Scaffold(body: SafeArea(child: RegisterPage())),
       ),
     );
   }
